4d056bc2f730b837bedd93cec51f5b63f01c7ac9
Blake Carey <151703120+slbcarey@users.noreply.github.com>
2024-01-22 21:28:44 -0500

    added js and css files to app components

---
+++ src/components/SearchResults/SearchResults.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './SearchResults.css';
+
+
+const SearchResults = () => {
+    
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+
+export default SearchResults;
