d385a07a8f728286a29eff9249e1cf40c6530455
Blake Carey <151703120+slbcarey@users.noreply.github.com>
2024-01-22 21:05:36 -0500

    created search bar in searchbar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,2 +1,29 @@
-import React from "react";
-import SearchBar from "react-native-search-bar";
+import React, { useState } from "react";
+
+const SearchBar = () => {
+    
+    const [search, setSearch] = useState("");
+
+    const handleInputChange = (e) => {
+        setSearch(e.target.value);
+    };
+
+    const handleSearch = () => {
+        console.log('Searching', search);
+    };
+
+    return (
+        <div className="Search-Bar-Container">
+            <input 
+            type="text" 
+            placeholder="Search"
+            value={search}
+            onChange={handleInputChange} />
+            <button onClick={handleSearch}>Search</button>
+        </div>
+    );
+};
+
+
+
+export default SearchBar;
