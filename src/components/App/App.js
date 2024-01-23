1088a9a24355ea371e5a52ddccfc45e5e52b2c3d
Blake Carey <151703120+slbcarey@users.noreply.github.com>
2024-01-22 21:18:26 -0500

    added base search bar to app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <SearchBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit fire <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
